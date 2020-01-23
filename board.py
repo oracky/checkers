4e144435228184344a69aa7a91b81a51842b8c01
Michal <michaloracki@wp.pl>
2020-01-23 13:17:02 +0100

    Delete all methods (unused class)

--- board.py
+++ board.py
@@ -1,20 +1,3 @@
-class Board:
-    def __init__(self, pl1_value, pl2_value):
-        self.player1 = pl1_value
-        self.player2 = pl2_value
-        self.player1_queen = -self.player1
-        self.player2_queen = -self.player2
-        self.empty = 0
-        self.board = [
-            [self.empty, self.player2, self.empty, self.player2, self.empty, self.player2, self.empty, self.player2],
-            [self.player2, self.empty, self.player2, self.empty, self.player2, self.empty, self.player2, self.empty],
-            [self.empty, self.player2, self.empty, self.player2, self.empty, self.player2, self.empty, self.player2],
-            [self.empty for i in range(0, 8)],
-            [self.empty for i in range(0, 8)],
-            [self.player1, self.empty, self.player1, self.empty, self.player1, self.empty, self.player1, self.empty],
-            [self.empty, self.player1, self.empty, self.player1, self.empty, self.player1, self.empty, self.player1],
-            [self.player1, self.empty, self.player1, self.empty, self.player1, self.empty, self.player1, self.empty]
-        ]
-
-    def update_board(self):
-        pass
+class Board():
+    def __init__(self):
+     pass
